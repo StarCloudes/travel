adff405d3ac21944efb8585435ec14204cf860a7
starcloudes <starcloudes@gmail.com>
2019-01-23 19:40:35 +0800

    <feat>增加homepage其它模块内容

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,6 +3,8 @@
     <home-header></home-header>
     <home-swiper :list="swiperList"></home-swiper>
     <home-icon :list="iconList"></home-icon>
+    <home-recommend :list="recommendList"></home-recommend>
+    <home-weekend :list="weekendList"></home-weekend>
   </div>
 </template>
 
@@ -10,6 +12,8 @@
 import HomeHeader from './components/HomeHeader.vue'
 import HomeSwiper from './components/Swiper.vue'
 import HomeIcon from './components/Icons.vue'
+import HomeRecommend from './components/Recommend.vue'
+import HomeWeekend from './components/Weekend.vue'
 import axios from "axios";
 
 export default {
@@ -17,13 +21,17 @@ export default {
   components:{
     HomeHeader,
     HomeSwiper,
-    HomeIcon
+    HomeIcon,
+    HomeRecommend,
+    HomeWeekend
   },
   data () {
     return {
       lastCity: '',
       swiperList: [],
-      iconList: []
+      iconList: [],
+      recommendList: [],
+      weekendList: []
     }
   },
   mounted () {
@@ -39,6 +47,8 @@ export default {
         const data = res.data
         this.swiperList = data.swiperList
         this.iconList = data.iconList
+        this.recommendList = data.recommendList
+        this.weekendList = data.weekendList
       }
     }
   }
