9d050c02b21a626bf2b81ec68ee29e8c365a6ccb
starcloudes <starcloudes@gmail.com>
2019-01-23 17:43:53 +0800

    <feat>add some templete to homepage

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <home-header></home-header>
+    <home-swiper></home-swiper>
+    <home-icon></home-icon>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/HomeHeader.vue'
+import HomeSwiper from './components/Swiper.vue'
+import HomeIcon from './components/Icons.vue'
+export default {
+  name: 'Home',
+  components:{
+    HomeHeader,
+    HomeSwiper,
+    HomeIcon
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgURL">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+         pagination: {
+            el: '.swiper-pagination',
+          },
+         loop: true,
+         autopaly: 3000
+      },
+      swiperList:[{
+          id:'001',
+          imgURL:'http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg'
+        },
+        {
+          id:'002',
+          imgURL:'http://img1.qunarzz.com/piao/fusion/1803/41/1b93b76d7f197c02.jpg_640x200_971065c4.jpg'
+        },
+      ]
+    }
+  }
+}
+
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
